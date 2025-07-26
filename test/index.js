cf8db23bdc9d27630611ffea97cfc39d7fdac5ad
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-07-27 01:40:33 +0700

    feat(test/index.js): add unit testing

---
+++ test/index.js
@@ -0,0 +1,13 @@
+const fun = require('../tool/bin/index.js')
+
+const ans = fun.getArgs([
+  '/home/mrezaananta/.local/share/mise/installs/node/24.3.0/bin/node',
+  '/home/mrezaananta/.local/share/mise/installs/node/24.3.0/bin/tool',
+  'asdk',
+  '--shadas=jafsd',
+  '-Uifgasd',
+  'notice',
+])
+console.log(ans)
+
+// console.assert()
