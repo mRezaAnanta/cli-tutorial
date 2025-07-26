d48caf48d0fc1a8773642930598a816d994f6a0d
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-07-27 01:38:11 +0700

    feat(tool/bin/index.js): add a way to get the arg from cli

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -2,28 +2,43 @@
 // console.log('hello tool')
 const process = require('process')
 
-export function getArgs() {
-  // return process.argv.forEach((val, index) => {
-  //   console.log(index + ": " + val)
-  // })
-    // const arr = []
-    const arr = {}
-  process.argv.map((args, index) => {
-    console.log(args, index)
+// FIX: the process.argv gets mixed up with the test/index.js
+function getArgs(input) {
+  // console.log(input + "2")
+  const arr = []
+  const obj = {}
+
+  input.splice(0, 2)
+  input.map((args, index) => {
+    // console.log(args, index)
     if(args.slice(0, 2) === "--") {
       // arr.push(args)
       const arg = args.split("=")
-      console.log(arg)
+      // console.log(arg)
       const argFlag = arg[0].slice(2)
       // console.log(argFlag)
       const argValue = arg.length > 1 ? arg[1] : true
       // console.log(argValue)
+      obj[argFlag] = argValue
+    }
+    else if(args[0] === "-") {
+      const flags = args.slice(1).split("")
+      flags.forEach((flag) => {
+        obj[flag] = true
+      })
+    }
+    else {
+      // obj['arg'] = args
+      arr.push(args)
     }
   })
-  console.log(arr)
-  return arr
+  // console.log(arr)
+  obj.arg = arr
+  return obj
 }
 
-// const args = getArgs()
-// console.log(args)
-getArgs()
+getArgs(process.argv)
+
+module.exports = {
+  getArgs
+}
