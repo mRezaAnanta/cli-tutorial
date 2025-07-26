2852957de3777943d611795b3de15dcbff0f9a53
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-07-26 08:26:23 +0700

    feat(tool/bin/index.js): add function to take argument from cli

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
-console.log('hello tool')
+// console.log('hello tool')
+const process = require('process')
+
+export function getArgs() {
+  // return process.argv.forEach((val, index) => {
+  //   console.log(index + ": " + val)
+  // })
+    // const arr = []
+    const arr = {}
+  process.argv.map((args, index) => {
+    console.log(args, index)
+    if(args.slice(0, 2) === "--") {
+      // arr.push(args)
+      const arg = args.split("=")
+      console.log(arg)
+      const argFlag = arg[0].slice(2)
+      // console.log(argFlag)
+      const argValue = arg.length > 1 ? arg[1] : true
+      // console.log(argValue)
+    }
+  })
+  console.log(arr)
+  return arr
+}
+
+// const args = getArgs()
+// console.log(args)
+getArgs()
