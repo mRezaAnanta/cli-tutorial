d8153a2e9719fc933c6c8b52d90c19efe238d03f
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-07-28 11:32:19 +0700

    test(test/index.js): add checkArg function to check if command exist in args

--- test/index.js
+++ test/index.js
@@ -1,13 +1,16 @@
-const fun = require('../tool/bin/index.js')
+const tool = require('../tool/bin/index.js')
 
-const ans = fun.getArgs([
+const argList = [
   '/home/mrezaananta/.local/share/mise/installs/node/24.3.0/bin/node',
   '/home/mrezaananta/.local/share/mise/installs/node/24.3.0/bin/tool',
   'asdk',
   '--shadas=jafsd',
   '-Uifgasd',
   'notice',
-])
-console.log(ans)
+  '--start',
+]
+
+const ans = tool.getArgs(argList)
+const check = tool.checkArg(ans, tool.commandList)
 
 // console.assert()
